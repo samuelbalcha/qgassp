cc9a76b3d07dbc013524ab207ed316c331f4b0e6
Samuel <samuel.balcha@gmail.com>
2021-05-23 19:06:21 +0300

    Linting fixes (#2)

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
-  isProduction: false,
-  isDevelopment: false,
-  isDemo: false,
-  TOKEN: 'co2-analyzer-token',
-  LOCAL_USER: 'co2-analyzer-user',
-  BASE_URL: 'http://localhost:3005',
-  GOOGLE_OAUTH_CLIENTID:
-    '655571057867-1lfpksd86ej8bnh0memh67ohmssj74ae.apps.googleusercontent.com',
+	isProduction: false,
+	isDevelopment: false,
+	isDemo: false,
+	TOKEN: 'co2-analyzer-token',
+	LOCAL_USER: 'co2-analyzer-user',
+	BASE_URL: 'http://localhost:3005',
+	GOOGLE_OAUTH_CLIENTID:
+		'655571057867-1lfpksd86ej8bnh0memh67ohmssj74ae.apps.googleusercontent.com',
 };
