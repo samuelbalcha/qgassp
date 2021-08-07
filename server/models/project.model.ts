03773c82bf8aa5737680b61aa5ab54d5bc31ee01
samuelbalcha <samuel.balcha@gmail.com>
2021-08-07 17:21:03 +0300

    Added project api to routes

--- server/models/project.model.ts
+++ server/models/project.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { ProjectStatuses } from '../../commons/enums/projectStatuses';
 import { ProjectTypes } from '../../commons/enums/projectTypes';
 
 export const ProjectSchema = new Schema({
@@ -7,6 +8,16 @@ export const ProjectSchema = new Schema({
 		required: true,
 		enum: [ProjectTypes.TERRITORIAL, ProjectTypes.CONSUMPTION],
 		default: ProjectTypes.TERRITORIAL,
+	},
+	status: {
+		type: String,
+		required: true,
+		enum: [
+			ProjectStatuses.DRAFT,
+			ProjectStatuses.ACTIVE,
+			ProjectStatuses.ARCHIVED,
+		],
+		default: ProjectStatuses.DRAFT,
 	},
 	name: {
 		type: String,
