228fc05d7a80c8750d55013e78631888797e6664
Samuel <samuel.balcha@gmail.com>
2021-11-10 12:43:01 +0300

    Feature/building energy calculator (#41)
    * Building energy baseline calculations

--- src/app/shared/components/building-energy/building-energy-result/building-energy-result.component.ts
+++ src/app/shared/components/building-energy/building-energy-result/building-energy-result.component.ts
@@ -2,6 +2,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType } from 'chart.js';
 import { Label, SingleDataSet } from 'ng2-charts';
+import _ from 'lodash';
+
+import { IProject } from '../../../../../../commons/types/IProject';
+import { ProjectService } from '../../../../core/services/project.service';
+import { UtilService } from '../../../../core/services/util.service';
 
 @Component({
 	selector: 'building-energy-result',
@@ -16,18 +21,42 @@ export class BuildingEnergyResultComponent implements OnInit {
 			display: true,
 		},
 	};
-	public pieChartLabels: Label[] = [
-		['Coal'],
-		['Peat'],
-		['Wood'],
+
+	public residentialHousingLabel: Label[] = [
+		['Apartment'],
+		['Terraced'],
+		['Semi-detached'],
+		['Detached'],
+	];
+
+	public commercialHousingLabel: Label[] = [
+		['Retal'],
+		['Health'],
+		['Hospitality'],
+		['Offices'],
+		['Industrial'],
+		['Warehouses'],
+	];
+
+	public fuelTypesLabel: Label[] = [
 		['Electricity'],
 		['Gas'],
 		['Oil'],
+		['Coal'],
+		['Peat'],
+		['Wood'],
+		['Renewable'],
+		['Heat'],
 	];
-	public pieChartData: SingleDataSet = [0.33, 0.09, 3, 26, 25, 46];
+
+	public shareOfResidentialHousingData: SingleDataSet = [];
+	public shareOfCommercialHousingData: SingleDataSet = [];
+	public shareOfResidentialEmissionByFuelTypeData: SingleDataSet = [];
+	public shareOfCommercialEmissionByFuelTypeData: SingleDataSet = [];
 	public pieChartType: ChartType = 'pie';
 	public pieChartLegend = true;
 	public pieChartPlugins = [];
+
 	public horiChartOptions: ChartOptions = {
 		responsive: false,
 		title: {
@@ -46,134 +75,101 @@ export class BuildingEnergyResultComponent implements OnInit {
 	public horiChartData: SingleDataSet = [50, -200, 150];
 	public horiChartType: ChartType = 'horizontalBar';
 
-	public residentialData = [
-		{
-			EnergyUse: 'Apartments',
-			Electricity: 124578933,
-			Gas: 1,
-			Oil: 2,
-			Coal: 12345,
-			Peat: 1234,
-			Wood: 123455,
-			Renewable: 1234,
-			Heat: 1232344,
-		},
-		{
-			EnergyUse: 'Terrace',
-			Electricity: 124578933,
-			Gas: 1,
-			Oil: 2,
-			Coal: 12345,
-			Peat: 1234,
-			Wood: 123455,
-			Renewable: 1234,
-			Heat: 1232344,
-		},
-		{
-			EnergyUse: 'Semi-detached',
-			Electricity: 124578933,
-			Gas: 2,
-			Oil: 2,
-			Coal: 12345,
-			Peat: 1234,
-			Wood: 123455,
-			Renewable: 1234,
-			Heat: 1232344,
-		},
-		{
-			EnergyUse: 'Detached',
-			Electricity: 1,
-			Gas: 1,
-			Oil: 1,
-			Coal: 1,
-			Peat: 1,
-			Wood: 1,
-			Renewable: 1,
-			Heat: 1,
-		},
-	];
+	public project: any;
 
-	public commercialData = [
-		{
-			EnergyUse: 'Retal',
-			Electricity: 124578933,
-			Gas: 1,
-			Oil: 2,
-			Coal: 12345,
-			Peat: 1234,
-			Wood: 123455,
-			Renewable: 1234,
-			Heat: 1232344,
-		},
-		{
-			EnergyUse: 'Health',
-			Electricity: 124578933,
-			Gas: 1,
-			Oil: 2,
-			Coal: 12345,
-			Peat: 1234,
-			Wood: 123455,
-			Renewable: 1234,
-			Heat: 1232344,
-		},
-		{
-			EnergyUse: 'Hospitality',
-			Electricity: 124578933,
-			Gas: 2,
-			Oil: 2,
-			Coal: 12345,
-			Peat: 1234,
-			Wood: 123455,
-			Renewable: 1234,
-			Heat: 1232344,
-		},
-		{
-			EnergyUse: 'Offices',
-			Electricity: 1,
-			Gas: 1,
-			Oil: 1,
-			Coal: 1,
-			Peat: 1,
-			Wood: 1,
-			Renewable: 1,
-			Heat: 1,
-		},
-		{
-			EnergyUse: 'Industrial',
-			Electricity: 1,
-			Gas: 1,
-			Oil: 1,
-			Coal: 1,
-			Peat: 1,
-			Wood: 1,
-			Renewable: 1,
-			Heat: 1,
-		},
-		{
-			EnergyUse: 'Warehouses',
-			Electricity: 1,
-			Gas: 1,
-			Oil: 1,
-			Coal: 1,
-			Peat: 1,
-			Wood: 1,
-			Renewable: 1,
-			Heat: 1,
-		},
-		{
-			EnergyUse: 'Other',
-			Electricity: 1,
-			Gas: 1,
-			Oil: 1,
-			Coal: 1,
-			Peat: 1,
-			Wood: 1,
-			Renewable: 1,
-			Heat: 1,
-		},
-	];
+	constructor(
+		private projectService: ProjectService,
+		private utilService: UtilService
+	) {}
 
 	ngOnInit(): void {
-		console.log('residentialData', this.residentialData);
+		this.project = this.projectService.getDraftProject() as IProject;
+		this.project = this.utilService.getBuildingEnergyBaselineResult(
+			this.project
+		);
+		this.projectService.updateDraftProject(this.project);
+		this.initPieCharts();
+	}
+
+	initPieCharts(): void {
+		const totalResidentialUnits = _.sumBy(
+			this.project.territorial.buildings.baseline.residentialBuildings,
+			'numberOfUnits'
+		);
+
+		this.shareOfResidentialHousingData = _.map(
+			this.project.territorial.buildings.baseline.residentialBuildings,
+			(item) => {
+				const share =
+					(item.numberOfUnits / totalResidentialUnits) * 100;
+				return Math.round(share);
+			}
+		);
+
+		const totalCommercialFloors = _.sumBy(
+			this.project.territorial.buildings.baseline.commercialBuildings,
+			'totalFloorArea'
+		);
+
+		this.shareOfCommercialHousingData = _.map(
+			this.project.territorial.buildings.baseline.commercialBuildings,
+			(item) => {
+				const share =
+					(item.totalFloorArea / totalCommercialFloors) * 100;
+				return Math.round(share);
+			}
+		);
+
+		this.shareOfResidentialEmissionByFuelTypeData = _.map(
+			this.calculateResidentialEnergyEmissionShare(
+				this.project.territorial.buildings.baseline.residentialBuildings
+			),
+			(item) => {
+				return item * 100;
+			}
+		);
+
+		this.shareOfCommercialEmissionByFuelTypeData = _.map(
+			this.calculateResidentialEnergyEmissionShare(
+				this.project.territorial.buildings.baseline.commercialBuildings
+			),
+			(item) => {
+				return item * 100;
+			}
+		);
+	}
+
+	calculateResidentialEnergyEmissionShare(buildingType: any): number[] {
+		const totalEnergyEmission = _.sumBy(
+			buildingType,
+			'totalEnergyEmission'
+		);
+
+		const electricityShare = _.sumBy(
+			buildingType,
+			'emissionResult.Electricity'
+		);
+
+		const gasShare = _.sumBy(buildingType, 'emissionResult.Gas');
+		const oilShare = _.sumBy(buildingType, 'emissionResult.Oil');
+		const coalShare = _.sumBy(buildingType, 'emissionResult.Coal');
+		const peatShare = _.sumBy(buildingType, 'emissionResult.Peat');
+		const woodShare = _.sumBy(buildingType, 'emissionResult.Wood');
+		const renewableShare = _.sumBy(
+			buildingType,
+			'emissionResult.Renewable'
+		);
+		const heatShare = _.sumBy(buildingType, 'emissionResult.Heat');
+
+		return [
+			Math.round((electricityShare / totalEnergyEmission) * 100) / 100,
+			Math.round((gasShare / totalEnergyEmission) * 100) / 100,
+			Math.round((oilShare / totalEnergyEmission) * 100) / 100,
+			Math.round((coalShare / totalEnergyEmission) * 100) / 100,
+			Math.round((peatShare / totalEnergyEmission) * 100) / 100,
+			Math.round((woodShare / totalEnergyEmission) * 100) / 100,
+			Math.round((renewableShare / totalEnergyEmission) * 100) / 100,
+			Math.round((heatShare / totalEnergyEmission) * 100) / 100,
+		];
 	}
 }
